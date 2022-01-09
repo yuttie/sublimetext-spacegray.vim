4f83b41c0f09807a2662cafc0bb415b43f9a5192
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2022-01-09 20:19:22 +0900

    Initial version
    Using the following Mustache template as a reference:
    * https://github.com/chriskempson/base16-textmate/blob/1d9e52afb74e03f5a2fdc53d9327428fe00c70c7/templates/default.mustache
    
    Colors are taken from:
    * https://github.com/kkga/spacegray/blob/8d4a59de254a27cc7c588c07a6a86662c0cf48d8/base16-ocean.dark.tmTheme
    * https://github.com/kkga/spacegray/blob/8d4a59de254a27cc7c588c07a6a86662c0cf48d8/base16-ocean.light.tmTheme
    
    And rendered into Vim colorscheme with:
    * https://github.com/chriskempson/base16-vim/blob/6191622d5806d4448fa2285047936bdcee57a098/templates/default.mustache

---
+++ render.py
@@ -0,0 +1,18 @@
+import json
+
+import chevron
+
+with open('template.mustache', 'r') as f:
+    template_vim = f.read()
+
+with open('spacegray-dark.json', 'r') as f:
+    def_dark = json.load(f)
+
+with open('spacegray-light.json', 'r') as f:
+    def_light = json.load(f)
+
+with open('sublimetext-spacegray-dark.vim', 'w') as f:
+    print(chevron.render(template_vim, def_dark), file=f)
+
+with open('sublimetext-spacegray-light.vim', 'w') as f:
+    print(chevron.render(template_vim, def_light), file=f)
